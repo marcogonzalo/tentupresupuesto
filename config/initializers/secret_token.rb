3abe31be0dfde6614e04bf18f9ee16b080ff38c1
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2012-10-04 18:28:18 -0430

    Proyecto inicial con bootstrap, locale es-VE, inflectores es, MySQL, debugger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttp::Application.config.secret_token = '9a0c401479a0e422876e6b39a846206f10116041cba515b5f9dc51461bc8147eeea98ede7f4e6188f176d9cf898165f948f151fa8737f65053810cc4b85912ff'
