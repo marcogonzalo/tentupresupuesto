7ac2ca07e2d640a54136f57815ce732c1b90fe31
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2012-12-09 21:01:53 -0430

    Compresor js y css, ruta a gliphicons, color blanco botones nav

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,9 @@ Ttp::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :yui
 
   # Expands the lines which load the assets
   config.assets.debug = false
