244734fadb0834b3871572d8515dbde1d0fd2f6f
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2012-12-12 23:16:08 -0430

    Mostrar proveedores que presentan presupuesto en vista de solicitud

--- app/helpers/presupuestos_helper.rb
+++ app/helpers/presupuestos_helper.rb
@@ -1,2 +1,17 @@
 module PresupuestosHelper
+  def mostrar_reputacion(cantidad, tipo = "general")
+    case tipo
+    when "trabajo"
+      rep = "reputacion-trabajo rt"
+    when "aspecto"
+      rep = "reputacion-aspecto ra"
+    else
+      rep = "reputacion-proveedor rp"
+    end
+    entero = cantidad.floor
+    decimal = (cantidad-entero>=0.5)?"_5":"_0"
+    borrar = ""
+    entero.times { |i| borrar += " * " }
+    return '<span class="'+rep+(entero.to_s)+decimal+'" title="'+(cantidad.to_s)+'"> '+borrar+'</span>'
+  end
 end
