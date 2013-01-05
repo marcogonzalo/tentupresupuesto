f6546056b48aa6b3872d69dd0fdc692e472d5241
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-01-05 22:32:55 +0100

    Pasar a aprobado=falso los demas presupuestos de un trabajo si ya uno ha sido aprobado

--- app/models/presupuesto_observer.rb
+++ app/models/presupuesto_observer.rb
@@ -2,9 +2,20 @@
 class PresupuestoObserver < ActiveRecord::Observer
   # Incrementar cantidad de solicitudes atendidas cuando el proveedor
   # presenta un nuevo presupuesto
-  def after_save(presupuesto)
+  def after_create(presupuesto)
     if presupuesto.proveedor_id.present?
       Proveedor.increment_counter(:solicitudes_atendidas,presupuesto.proveedor_id)
+    end
+  end
+  
+  def after_update(presupuesto)
+    if presupuesto.aprobado and presupuesto.trabajo_id.present?
+      presupuestos = Presupuesto.where(:trabajo_id => presupuesto.trabajo_id, :aprobado => nil)
+      presupuestos.each do |p|
+        p.aprobado = false
+        p.rechazado = false
+        p.save
+      end
     end
   end
 
