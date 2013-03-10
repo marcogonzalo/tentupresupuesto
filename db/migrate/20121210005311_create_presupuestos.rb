4441b38096681c72e7faf460bb0997ebc85f56f5
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-03-10 15:54:11 -0430

    contador de presupuestos en trabajo y contador de mensajes en presupuesto

--- db/migrate/20121210005311_create_presupuestos.rb
+++ db/migrate/20121210005311_create_presupuestos.rb
@@ -1,14 +1,15 @@
 class CreatePresupuestos < ActiveRecord::Migration
   def change
     create_table :presupuestos do |t|
-      t.float   :precio_minimo,   :null => false, :default => 0.0, :precision => 8, :scale => 2
-      t.float   :precio_maximo,   :null => false, :default => 0.0, :precision => 8, :scale => 2
-      t.text    :resumen,         :null => false, :default => ""
-      t.boolean :con_iva,         :null => false, :default => true
-      t.boolean :visto,           :null => false, :default => false
-      t.boolean :aprobado,        :null => true, :default => nil
-      t.boolean :rechazado,       :null => false, :default => false
-      t.string  :motivo_rechazo,  :null => true, :limit => 20, :default => nil
+      t.float   :precio_minimo,     :null => false, :default => 0.0, :precision => 8, :scale => 2
+      t.float   :precio_maximo,     :null => false, :default => 0.0, :precision => 8, :scale => 2
+      t.text    :resumen,           :null => false, :default => ""
+      t.boolean :con_iva,           :null => false, :default => true
+      t.boolean :visto,             :null => false, :default => false
+      t.boolean :aprobado,          :null => true, :default => nil
+      t.boolean :rechazado,         :null => false, :default => false
+      t.string  :motivo_rechazo,    :null => true, :limit => 20, :default => nil
+      t.integer :cant_mensajes,     :null => false, :default => 0
 
       t.timestamps
     end
