1d89ce08e6c116300a93350e6e6cb158e127765b
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-04-07 21:21:49 -0430

    ajustes de ambiente

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Ttp::Application.configure do
   # Precompile additional assets
   config.assets.precompile += ['*.svg', '*.eot', '*.woff', '*.ttf', '*.otf', '*.js', '*.css']
   # %w( .svg .eot .woff .ttf .otf .js .css)
+  config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w(ubicaciones_geograficas.js proveedores.js solicitantes.js presupuestos.js trabajos.js)
