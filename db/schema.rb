5f9ac9a0f3c91a0275a6662d071369c9421ee137
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-04-07 23:20:30 -0430

    ajustes de ambiente

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407040457) do
+ActiveRecord::Schema.define(:version => 20130407043229) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -141,6 +141,19 @@ ActiveRecord::Schema.define(:version => 20130407040457) do
   add_index "proveedores", ["pais_id"], :name => "index_proveedores_on_pais_id"
   add_index "proveedores", ["rif"], :name => "index_proveedores_on_rif", :unique => true
   add_index "proveedores", ["slug"], :name => "index_proveedores_on_slug", :unique => true
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "solicitantes", :force => true do |t|
     t.string   "cedula",                 :limit => 20, :default => "", :null => false
