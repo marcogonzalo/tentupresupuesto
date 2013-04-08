8e9a16be528745e7a68536add2c819f56dc87946
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-04-08 00:54:22 -0430

    ajustes de ambiente

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,9 +53,9 @@ Ttp::Application.configure do
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   
   # Precompile additional assets
-  config.assets.precompile = ['*.svg', '*.eot', '*.woff', '*.ttf', '*.otf', '*.js', '*.ico']
+  config.assets.precompile = ['*.svg', '*.eot', '*.woff', '*.ttf', '*.otf', '*.js', '*.ico', '*.png', '*.jpg', '*.jpeg', '*.gif']
   config.assets.precompile += ['base_ttp*', 'application.css', 'devise.css', 'email.css', 'errores.css', 'general.css', 'interna-liston.css', 'landing.css']
-  config.assets.precompile += ['categorias*', 'mensajes*', 'presupuestos*', 'proveedores*', 'reputacion*', 'trabajos*', 'ubicaciones_geograficas*']
+  config.assets.precompile += ['categoria*', 'mensajes*', 'presupuestos*', 'proveedores*', 'reputacion*', 'trabajos*', 'ubicaciones_geograficas*']
   config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
   # %w( .svg .eot .woff .ttf .otf .js .css)
 
