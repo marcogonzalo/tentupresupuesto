86231fb9ce26dd9ad5b8fd348b2d95276f3173a8
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-04-08 00:22:34 -0430

    ajustes de ambiente

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,8 +54,8 @@ Ttp::Application.configure do
   
   # Precompile additional assets
   config.assets.precompile = ['*.svg', '*.eot', '*.woff', '*.ttf', '*.otf', '*.js']
-  config.assets.precompile += ['base_ttp.css*', 'application.css', 'devise.css', 'email.css', 'errores.css', 'general.css', 'interna-liston.css', 'landing.css']
-  config.assets.precompile += ['categoria.css*', 'mensajes.css*', 'presupuestos.css*', 'proveedores.css*', 'reputacion.css*', 'trabajos.css*', 'ubicaciones_geograficas.css*']
+  config.assets.precompile += ['base_ttp*', 'application.css', 'devise.css', 'email.css', 'errores.css', 'general.css', 'interna-liston.css', 'landing.css']
+  config.assets.precompile += ['categoria*', 'mensajes*', 'presupuestos*', 'proveedores*', 'reputacion*', 'trabajos*', 'ubicaciones_geograficas*']
   config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
   # %w( .svg .eot .woff .ttf .otf .js .css)
 
