6c7c01bac5fd28dfd28e7f3c4029325b2e685ff6
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-05-07 22:49:48 -0430

    desactivar campos de ubicacion al cargar formulario

--- app/assets/javascripts/solicitantes.js
+++ app/assets/javascripts/solicitantes.js
@@ -1,31 +1,42 @@
+// Registro de proveedor
 $(document).ready(function($) {// Carga de lista inicial de localidades en municipio seleccionado
-	cargar_entidades('solicitante', 'localidad', 'municipio');
+    cargar_entidades('solicitante', 'localidad', 'municipio');
+    if(!_editando) {
+        setTimeout('desactivar()',1500);
+    }
 });
 
+function desactivar() {
+    $("#solicitante_municipio_id").attr('disabled', 'disabled');
+    $("#solicitante_localidad_id").attr('disabled', 'disabled');
+}
+
 $('#solicitante_pais_id').change(function() {
-	$("#solicitante_estado_id").attr('disabled', 'disabled');
-	$("#solicitante_municipio_id").attr('disabled', 'disabled');
-	$("#solicitante_localidad_id").attr('disabled', 'disabled');
-	
-	if ($('#solicitante_pais_id').val() != "") {
-		cargar_entidades('solicitante', 'estado', 'pais');
-	}
+    $("#solicitante_estado_id").attr('disabled', 'disabled');
+    $("#solicitante_municipio_id").attr('disabled', 'disabled');
+    $("#solicitante_localidad_id").attr('disabled', 'disabled');
+
+    $("#solicitante_localidad_id").val("");
+    if ($('#solicitante_pais_id').val() != "") {
+        cargar_entidades('solicitante', 'estado', 'pais');
+    }
 });
 
 $('#solicitante_estado_id').change(function() {
-	$("#solicitante_municipio_id").attr('disabled', 'disabled');
-	$("#solicitante_localidad_id").attr('disabled', 'disabled');
-	
-	if ($('#solicitante_estado_id').val() != "") {
-		cargar_entidades('solicitante', 'municipio', 'estado');
-	}
+    $("#solicitante_municipio_id").attr('disabled', 'disabled');
+    $("#solicitante_localidad_id").attr('disabled', 'disabled');
+
+    $("#solicitante_localidad_id").val("");
+    if ($('#solicitante_estado_id').val() != "") {
+        cargar_entidades('solicitante', 'municipio', 'estado');
+    }
 });
 
 $('#solicitante_municipio_id').change(function() {
-	$("#solicitante_localidad_id").attr('disabled', 'disabled');
-	
-	$("#solicitante_localidad_id").val("");
-	if ($('#solicitante_municipio_id').val() != "") {
-		cargar_entidades('solicitante', 'localidad', 'municipio');
-	}
-}); 
+    $("#solicitante_localidad_id").attr('disabled', 'disabled');
+
+    $("#solicitante_localidad_id").val("");
+    if ($('#solicitante_municipio_id').val() != "") {
+        cargar_entidades('solicitante', 'localidad', 'municipio');
+    }
+});
