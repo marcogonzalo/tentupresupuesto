6df1655f2e4e68e92ba1e89a00f8d1e3a3335fa4
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-05-20 00:29:19 -0430

    Ajustes en rechazo de presupuesto

--- app/assets/javascripts/presupuestos.js
+++ app/assets/javascripts/presupuestos.js
@@ -1,41 +1,38 @@
 jQuery(function($) {
-  $("#aceptar-presupuesto")
-    .bind("ajax:loading",  alternar_cargando("#loading-aprobar-rechazar"))
-    .bind("ajax:success", function(event, data, status, xhr) {
-      mostrar_mensaje("#mensaje-ttp","alert alert-"+data.tipo_mensaje,data.mensaje)
-      if(data.tipo_mensaje == "success") {
-        $("#aceptar-presupuesto").addClass("disabled")
-        $("#rechazar-presupuesto").addClass("disabled")
-        $("#aceptar-presupuesto").removeAttr('href')
-        $("#rechazar-presupuesto").removeAttr('href')
-        $("#aceptar-presupuesto").fadeOut()
-        $("#rechazar-presupuesto").fadeOut()
-      }
-      ocultar_cargando("#loading-aprobar-rechazar")
-    })
-    .bind("ajax:error", function(xhr, status, error) {
-      mostrar_mensaje("#mensaje-ttp","alert-error",error)
-      ocultar_cargando("#loading-aprobar-rechazar")
+    $("#aceptar-presupuesto").bind("ajax:loading", mostrar_cargando("#loading-aprobar-rechazar")).bind("ajax:success", function(event, data, status, xhr) {
+        mostrar_mensaje("#mensaje-ttp", "alert alert-" + data.tipo_mensaje, data.mensaje)
+        if (data.tipo_mensaje == "success") {
+            $("#aceptar-presupuesto").addClass("disabled")
+            $("#rechazar-presupuesto").addClass("disabled")
+            $("#aceptar-presupuesto").removeAttr('href')
+            $("#rechazar-presupuesto").removeAttr('href')
+            $("#aceptar-presupuesto").fadeOut()
+            $("#rechazar-presupuesto").fadeOut()
+        }
+        ocultar_cargando("#loading-aprobar-rechazar")
+    }).bind("ajax:error", function(xhr, status, error) {
+        mostrar_mensaje("#mensaje-ttp", "alert-error", error)
+        ocultar_cargando("#loading-aprobar-rechazar")
     });
 
-  $("#rechazo-presupuesto")
-    .bind("ajax:loading",  alternar_cargando("#loading-aprobar-rechazar"))
-    .bind("ajax:success", function(event, data, status, xhr) {
-      mostrar_mensaje("#mensaje-ttp","alert alert-"+data.tipo_mensaje,data.mensaje)
-      if(data.tipo_mensaje == "success") {
-        $("#aceptar-presupuesto").addClass("disabled")
-        $("#rechazar-presupuesto").addClass("disabled")
-        $("#aceptar-presupuesto").removeAttr('href')
-        $("#rechazar-presupuesto").removeAttr('href')
-        $("#aceptar-presupuesto").fadeOut()
-        $("#rechazar-presupuesto").fadeOut()
-      }
-      ocultar_cargando("#loading-aprobar-rechazar")
-      $("#modal-rechazo").modal('hide')
-    })
-    .bind("ajax:error", function(xhr, status, error) {
-      mostrar_mensaje("#mensaje-ttp","alert-error",error)
-      ocultar_cargando("#loading-aprobar-rechazar")
-      $("#modal-rechazo").modal('hide')
-    }); 
-});
+    $("#rechazo-presupuesto").bind("ajax:loading", mostrar_cargando(".cargando")).bind("ajax:success", function(event, data, status, xhr) {
+        mostrar_mensaje("#mensaje-ttp", "alert alert-" + data.tipo_mensaje, data.mensaje)
+        if (data.tipo_mensaje == "success") {
+            $("#aceptar-presupuesto").addClass("disabled")
+            $("#rechazar-presupuesto").addClass("disabled")
+            $("#aceptar-presupuesto").removeAttr('href')
+            $("#rechazar-presupuesto").removeAttr('href')
+            $("#aceptar-presupuesto").fadeOut()
+            $("#rechazar-presupuesto").fadeOut()
+        }
+        ocultar_cargando(".cargando")
+        //ocultar_cargando("#loading-aprobar-rechazar")
+        $("#modal-rechazo").modal('hide')
+    }).bind("ajax:error", function(xhr, status, error) {
+        mostrar_mensaje("#mensaje-ttp", "alert-error", error)
+        ocultar_cargando(".cargando")
+        //ocultar_cargando("#loading-aprobar-rechazar")
+        $("#modal-rechazo").modal('hide')
+    });
+    ocultar_cargando(".cargando")
+}); 
