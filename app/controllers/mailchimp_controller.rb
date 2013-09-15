21025dc19ea3efd202f1bb4c2b1182ef9fd4b928
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-09-15 00:00:20 -0430

    permitir sesion sin confirmar correo. Integracion con MailChimp

--- app/controllers/mailchimp_controller.rb
+++ app/controllers/mailchimp_controller.rb
@@ -41,4 +41,44 @@ class MailchimpController < ApplicationController
       end
     end
   end
+  
+  def self.unsubscribe(email,lista)
+    @mc = Mailchimp::API.new(MAILCHIMP_API_KEY)
+    case lista
+    when 'solicitantes'
+      list_id = 'd503ae082d'
+    when 'proveedores'
+      list_id = '6769b54e06'
+    end
+    begin
+      @mc.lists.unsubscribe(list_id, {'email' => email})
+      mensaje = "#{email} desuscrito correctamente"
+      return
+    rescue Mailchimp::ListNotSubscribedError
+      mensaje = "#{email} no suscrito a la lista"
+      return
+    rescue Mailchimp::ListDoesNotExistError
+      mensaje = "No se pudo conseguir la lista"
+      #redirect_to "/lists/"
+      return
+    rescue Mailchimp::Error => ex
+      if ex.message
+        mensaje = ex.message
+      else
+        mensaje = "Ha ocurrido un error desconocido"
+      end
+    end
+    logger.debug(mensaje)
+    puts("mensaje - "+mensaje)
+    #redirect_to "/lists/#{list_id}"
+    respond_to do |format|
+      if flash[:success]
+        format.json { render :json => { tipo_mensaje: :success, mensaje: mensaje}}
+      elsif flash[:warning]
+        format.json { render :json => { tipo_mensaje: :warning, mensaje: mensaje}}
+      else
+        format.json { render :json => { tipo_mensaje: :error, mensaje: mensaje}}
+      end
+    end
+  end
 end
--- config/initializers/constantes.rb
+++ config/initializers/constantes.rb
@@ -1,1 +1,2 @@
-MAILCHIMP_API_KEY = '86e3dd77a6e63c207f316344010fb7ae-us7'
+# Claves de APIs de Servicios Externos
+MAILCHIMP_API_KEY = '86e3dd77a6e63c207f316344010fb7ae-us7'      # Clave de API de MailChimp
