05e41e3a2b2a70b4e3901c775717a0e5c97b7472
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2013-12-03 23:34:37 -0430

    bug en enlaces sociales de perfil

--- app/assets/javascripts/proveedores_show.js
+++ app/assets/javascripts/proveedores_show.js
@@ -1,28 +1,27 @@
-
 // Perfil de proveedor
 $('#perfil #trabajos').hide();
 
-$('#perfil-tabs a').click(function (e) {
-  e.preventDefault();
-  href = $(this).attr('href');
-  	switch(href) {
-	    case '#informacion':
-	    	$('#perfil '+href).show();
-	    	$('#perfil #trabajos').hide(); 
-	    	$('#perfil #evaluaciones').hide(); 
-	    	$('#titulo-pestana h3').text("Información del proveedor")
-	  		break;
-	    case '#trabajos':
-	    	$('#perfil '+href).show();
-	    	$('#perfil #informacion').hide();
-	    	$('#perfil #evaluaciones').hide(); 
-	    	$('#titulo-pestana h3').text("Imágenes de trabajos")
-	    	break;
-	    case '#evaluaciones':
-	    	$('#perfil '+href).show();
-	    	$('#perfil #informacion').hide();
-	    	$('#perfil #trabajos').hide(); 
-	    	$('#titulo-pestana h3').text("Valoraciones")
-	    	break;
-	}
-})
+$('#perfil-tabs a').click(function(e) {
+    e.preventDefault();
+    href = $(this).attr('href');
+    switch(href) {
+        case '#informacion':
+            $('#perfil ' + href).show();
+            $('#perfil #trabajos').hide();
+            $('#perfil #evaluaciones').hide();
+            $('#titulo-pestana h3').text("Información del proveedor")
+            break;
+        case '#trabajos':
+            $('#perfil ' + href).show();
+            $('#perfil #informacion').hide();
+            $('#perfil #evaluaciones').hide();
+            $('#titulo-pestana h3').text("Imágenes de trabajos")
+            break;
+        case '#evaluaciones':
+            $('#perfil ' + href).show();
+            $('#perfil #informacion').hide();
+            $('#perfil #trabajos').hide();
+            $('#titulo-pestana h3').text("Valoraciones")
+            break;
+    }
+}); 
